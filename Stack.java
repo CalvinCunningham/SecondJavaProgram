ff4e7f468ffd789a46c97674faaf5e4c62134e6b
Calvin.Cunningham <calvin.a.cunningham@raytheon.com>
2018-08-29 08:07:09 -0400

    Fixed memory corruption issue in Second Program.

--- Stack.java
+++ Stack.java
@@ -6,12 +6,15 @@ public class Stack {
 	 * that can only access it members through operations.
 	 */
 	
-	final int MAX_VALUE = 100;
-	private int stck[] = new int[MAX_VALUE];
-	private int tos;
+	final private int MAX_VALUE = 100;
+	private int stck[] = new int[MAX_VALUE];	
+	private int tos;	
+	private int value;
+
 	
 	Stack ( ) {
 		tos = -1;
+		
 		
 	}
 	
@@ -31,21 +34,57 @@ public class Stack {
 			System.out.println("The stack underflow or empty");
 		    return 0;
 		}    
-		else 
-			return stck[tos--];
-				
+		else {
+			 value  = stck[tos--];
+			 					 
+			 System.out.println("currentValue  " + value);
+			 
+			 return value;
+			 
+		}
+		
+		
 	}
+			 
+		    
+	void resetStackContent() {
+		// While traversing an array or list modifying the array
+		// or list will corrupt the memory and give unwanted
+		// and unexpected results.
+		for( tos = (MAX_VALUE -1 ); tos > 0 ; tos--) {
+			
+		    if( tos > 0) {
+			
+			    System.out.println("tos: " + tos);
+			    stck[tos] = 0;
+		    }
+		
+		}
+		//Reset tos integer so we can add more items to the list.
+		tos = -1;
+		
+	}
+	   
+				
+	
 	
 	void printStack() {
 		
-		System.out.print("The contents of the stack are ");
+		System.out.print("The contents of the Stack1 are ");
 		for( int item: stck ) {
 			
 			 if( item > 0)
 			     System.out.print(item + "  ");
 		}
 	
-		System.out.println("Stack Complete.");
+		System.out.println("Stack1 Complete.");
+		
+		
+	}
+	
+	int getMaxValue() {
+		
+		return stck.length;
 	}
 	
 	
@@ -54,18 +93,39 @@ public class Stack {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to your Second Java Program.");
-		
+		int popReturnValue;
 		Stack stack = new Stack();
+
+		int maxValue = stack.getMaxValue();
 		
-		
-		
-		for( int index = 1; index < (stack.MAX_VALUE ); index++) {
+		for( int index=0; index <  maxValue; index++) {
 			
 			stack.push(index);
+			
 		}
 		
 		stack.printStack();
 		
+		
+		System.out.println("*********************Remove from Stack********************");
+		
+		for( int index=0; index <  maxValue; index++) {
+			
+			 popReturnValue = stack.pop();
+			 						 
+			 if( popReturnValue > 0)
+		         System.out.println("Stack Value removed " + popReturnValue);
+			 
+		
+		}
+		
+			
+		stack.resetStackContent();
+		
+		
+		stack.printStack();
+		
+
 		
 	}
 
